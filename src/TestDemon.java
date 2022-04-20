b4d379b394ff25e4356ebb8d1e1b4aa6b1019c27
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-04-20 13:55:18 +0800

    2022/04/20练习

---
+++ src/TestDemon.java
@@ -0,0 +1,36 @@
+public class TestDemon {
+    /**
+     * name：Wang wei
+     * date：2022/04/20
+     * 第二次Java练习
+     * 练习变量
+     * @param args
+     */
+    public static void main(String[] args) {
+        double x=2.0;
+        int y=4;
+        x/=++y;
+        System.out.println(x);
+        System.out.println("\\hello\\");
+        int a=3;
+        byte b=(byte)a;
+        System.out.println(b);
+        boolean c=true;
+        System.out.println(c);
+        String str="你好";
+        char arr=1000;
+        System.out.println(arr);
+        System.out.println(str);
+        byte m=127;
+        byte n;
+        n = (byte)(m+1);
+        System.out.println(n);
+        System.out.println(m);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Character.MAX_VALUE);
+        String num="こんにちは";
+        System.out.println(num);
+
+    }
+}
