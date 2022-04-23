adc5d677994d6a6270db723fe734a42e11c1d691
“01294268442” <1294268442@qq.com>
2022-04-23 10:40:36 +0800

    test

---
+++ Helloworld/src/hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class hello {
+    public static void main(String[] args){
+        Scanner scan =new Scanner(System.in);
+        int a=scan.nextInt();
+        double b=scan.nextDouble();
+        System.out.println("Hello,World");
+        System.out.println(a);
+        System.out.println(b);
+        byte m=0;
+        boolean f=true;
+        System.out.println(f);
+        System.out.println(m);
+    }
+}
---
+++ test1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test1{
+	public static void main(String[] args) {
+		 Scanner scan=new Scanner(System.in);
+		 //int a=scan.nextInt();
+		 //System.out.println(a);
+		 String str=scan.nextLine();
+		 System.out.println(str);
+
+	}
+}
