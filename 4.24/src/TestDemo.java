2a1af36ff193df8ef8663d14fb99e7f48cf116ef
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-04-24 16:47:23 +0800

    Add files via upload

--- 4.24/src/TestDemo.java
+++ 4.24/src/TestDemo.java
@@ -1,6 +1,46 @@
 import java.util.Scanner;
 public class TestDemo {
+    public static int func2(int n) {
+        if(n>9) {
+            return n%10+func2(n/10);
+        }else {
+           return n;
+        }
+    }
+    public static int feiBo(int n) {
+        if(n==1||n==2) {
+            return 1;
+        }else {
+            return feiBo(n-1)+feiBo(n-2);
+        }
+    }
     public static void main(String[] args) {
+        int m=func2(1729);
+        System.out.println(m);
+        int n=feiBo(9);
+        System.out.println(n);
+    }
+    public static void wei(int n) {
+        if(n>9) {
+             wei(n/10);
+        }
+        System.out.print(n%10+" ");
+    }
+    public static void main6(String[] args) {
+        wei(123456);
+    }
+    public static int fac(int n) {
+        if(n==1) {
+            return 1;
+        }else {
+            return n*fac(n-1);
+        }
+    }
+    public static void main5(String[] args) {
+        int m=fac(5);
+        System.out.println(m);
+    }
+    public static void main4(String[] args) {
         int count=0;
         for(int i=1;i<=100;i++) {
             if(i%10==9) {
