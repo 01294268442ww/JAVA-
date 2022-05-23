cc1adc4e46bf75714a8ff2295930b06f1b9cca26
01294268442ww <92193285+01294268442ww@users.noreply.github.com>
2022-05-23 09:51:30 +0800

    Add files via upload

---
+++ 5.23/5.23/源.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	/*int i = 0;
+	char ch = 0;
+	cin >> ch;
+	cin.ignore(100, 'B');
+	cin >> i;
+	cout << i << endl;
+	cout << ch << endl;
+	string _name;
+	getline(cin, _name);
+	cout << _name;*/
+
+	/*string firstName;
+	string lastName;
+	getline(cin, firstName);
+	getline(cin, lastName);
+
+	cout << firstName << endl;
+	cout << lastName << endl;*/
+
+
+
+	return 0;
+}
